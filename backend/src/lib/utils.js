9275ebbb997a18f839a74fe621e2a36fd47c8c4c
wyadi <wyadi0305@gmail.com>
2025-02-08 01:16:00 -0500

    Signup

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({id: userId}, process.env.JWT_SECRET, {
+        expiresIn: "7d"
+    });
+
+    const options = {
+        expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production" ? true : false
+    };
+
+    res.cookie("jwt", token, options);
+
+    return token;
+}
